108c2e512e02c1c6cff3ebeb7a277cbed0f1c10e
Wésley P.Martins <weslley2013.pinto@gmail.com>
2023-03-10 22:36:52 -0300

    Reparando a função de adição de operadores.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const operatorsList = ["-", "+", "/", "x", "%"]
+const operatorsList = ["-", "+", "÷", "x", "%"]
 const operatorBtns = document.querySelectorAll("[data-operator]");
 const numberBtns = document.querySelectorAll("[data-num]");
 const clearAllBtn = document.getElementById("clear-all");
@@ -27,25 +27,68 @@ function addNumber(numberBtn) {
 
 function addOperator(operatorBtn) {
     const operator = operatorBtn.getAttribute("data-operator");
+    const lastDigit = total.innerHTML[total.innerHTML.length - 1];
 
-    if(total.innerHTML.length > 0) {
-        innerOperators += operator;
+    const canAdd = total.innerHTML.length > 0 && !total.innerHTML.endsWith(operator);
 
-        total.innerHTML += operator;
+    if(canAdd) {
+        switch(lastDigit) {
+            case "-":
+            case "+":
+                innerOperators = innerOperators.slice(0, -1) + operator;
+                total.innerHTML = total.innerHTML.slice(0, -1) + operator;
+                break
+
+            case "÷":
+            case "x":
+                if(operator === "-") { 
+                    innerOperators += operator;
+                    total.innerHTML += operator;
+                }
+                else {
+                    innerOperators = innerOperators.slice(0, -1) + operator; 
+                    total.innerHTML = total.innerHTML.slice(0, -1) + operator;
+                }
+                break
+
+            default:
+                innerOperators += operator;
+                total.innerHTML += operator;
+        }
     }  
 }
 
 function clearAll() {
     firstNumber = "";
     secondNumber = "";
+    innerOperators = "";
     total.innerHTML = "";
 }
 
-// Arrumar essa função
 function clearDigit() {
-    firstNumber = firstNumber.slice(0, -1);
+    const lastDigit = total.innerHTML[total.innerHTML.length - 1];
+
     total.innerHTML = total.innerHTML.slice(0, -1);
+
+    if(innerOperators.endsWith(lastDigit)) {
+        innerOperators = innerOperators.slice(0, -1);
+        return
+    } else if (secondNumber.endsWith(lastDigit)) {
+        secondNumber = secondNumber.slice(0, -1);
+        return
+    }
+    else if (firstNumber.endsWith(lastDigit)) { 
+        firstNumber = firstNumber.slice(0, -1) 
+    }  
 }
+
+/*function generateResult() {
+    if(innerOperators.length === 1) {
+        switch(innerOperators) {
+            case "-"
+        }
+    }
+}*/
 
 for(let i = 0; i < numberBtns.length; i++) {
     numberBtns[i].addEventListener('click', function() { addNumber(numberBtns[i]) })
